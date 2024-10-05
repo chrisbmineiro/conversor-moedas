a0c2e80284419c6cb5b0540a460aff399079505b
chrisbmineiro <chrisbmineiro@gmail.com>
2024-10-04 23:06:00 -0300

    adicionando class Conversao para conversão de valores

---
+++ src/modelos/Conversao.java
@@ -0,0 +1,20 @@
+package modelos;
+
+public class Conversao {
+
+    private final TaxaCambioService taxaCambioService;
+
+    // Construtor que recebe o serviço de taxa de câmbio
+    public Conversao(TaxaCambioService taxaCambioService) {
+        this.taxaCambioService = taxaCambioService;
+    }
+
+    // Métod que realiza a conversão de moeda
+    public double converter(Moeda base, Moeda destino, double valor) {
+        double taxa = taxaCambioService.obterTaxaDeCambio(base, destino);
+        if (taxa != -1) {
+            return valor * taxa;
+        }
+        return -1;
+    }
+}
